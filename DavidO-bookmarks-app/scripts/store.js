c85ca5917ef644823cad5da97e53fdbfc1f94888
DOmstead <49734565+DOmstead@users.noreply.github.com>
2019-06-15 17:18:14 -0600

    Bookmarks app currently in progress
    This development of this app is still ongoing but wanted to upload my current progress before the submission deadline.

--- DavidO-bookmarks-app/scripts/store.js
+++ DavidO-bookmarks-app/scripts/store.js
@@ -5,6 +5,7 @@
 const store = (function(){
 
   const addItem = function(item) {
+    item.expanded = false;
     this.items.push(item);
   };
 
@@ -16,14 +17,22 @@ const store = (function(){
     this.items = this.items.filter(item => item.id !== id);
   };
 
+  // const expandingBookmarkToggle = function(id){
+  //   console.log('store.expandingBookmark Ran');
+  //   this.items.find(
+  //     items => items.id === 
+  //   );
+  //    = !this.expanded;
+  // };
+
   return{
     items: [],
     adding:false,
-    expanded:false,
     error: null,
     addItem,
     addBookmarkForm,
     findAndDelete,
+    // expandingBookmark
   };
 
 }());
